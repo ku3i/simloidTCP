4592fd865b0d8dc493c8edfb00bc4acc926ffb39
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2018-01-19 19:58:33 +0100

    Release 1.1.0

--- src/basic/version.h
+++ src/basic/version.h
@@ -6,8 +6,8 @@
 namespace version
 {
     const std::string  prog_name = "SimloidTCP";
-    const unsigned int major     = 1;     // interface changes
-    const unsigned int minor     = 0;     // feature changes
+    const unsigned int major     = 1;     // incompatible interface changes
+    const unsigned int minor     = 1;     // feature changes
     const unsigned int bugfixes  = 0;
     const std::string  name      = prog_name + " " + std::to_string(major)
                                              + "." + std::to_string(minor)
